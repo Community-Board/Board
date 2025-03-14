66e3290a95e3666e910336a56080a77fbc64f55d
Ryunos96 <sinple010@gmail.com>
2025-03-14 20:58:28 +0900

    first test commit

---
+++ src/main/java/com/example/small/SmallprojectApplication.java
@@ -0,0 +1,11 @@
+package com.example.small;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallprojectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmallprojectApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/small/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.small.controller;
+
+import com.example.small.service.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")  // GET 요청
+    public String hello() {
+        return helloService.getHelloMessage();  // 서비스에서 메시지 받아 반환
+    }
+}
