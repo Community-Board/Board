440311704995bab5d1764ff758c2ad76e0e4b5e5
YunYongWoon <yuyun0124@gmail.com>
2025-03-20 20:56:59 +0900

    feat : 게시글 목록 조회 API 구현

---
+++ src/main/java/com/example/yun/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.yun.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// DB -> Datasoruce -> mybatis -> (제목, 내용, 날짜) -> CRUD
+
+/**
+ * 1. 게시글 목록
+ * 2. 게시글 단건 조회
+ * 3. 게시글 작성
+ * 4. 게시글 수정
+ * 5. 게시글 삭제
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
